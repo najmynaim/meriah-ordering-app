18b77e6d4dc94bd95c763d336f8b38073ce9cff4
Your Name <you@example.com>
2022-01-25 10:19:39 +0800

    created orders page and css

--- pages/Cart.jsx
+++ pages/Cart.jsx
@@ -2,61 +2,95 @@ import styles from "../styles/Cart.module.css";
 import Image from "next/image";
 
 const Cart = () => {
-    return (
-        <div className={styles.container}>
-            <div className={styles.left}>
-                <table className={styles.table}> 
-                    <tr className={styles.tr}>
-                        <th>Product</th>
-                        <th>Name</th>
-                        <th>Extras</th>
-                        <th>Price</th>
-                        <th>Quantity</th>
-                        <th>Product</th>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div className={styles.imgContainer}>
-                                <Image src="/img/product.png" layout="fill" alt="" />
-                            </div>
-                        </td>
-                        <td>
-                            <span className={styles.name}>PRODUCT NAME</span>
-                        </td>
-                        <td>
-                            <span className={styles.extras}>
-                                Double extra, extra stuffs
-                            </span>
-                        </td>
-                        <td>
-                            <span className={styles.price}>$19.90</span>
-                        </td>
-                        <td>
-                            <span className={styles.quantity}>2</span>
-                        </td>
-                        <td>
-                            <span className={styles.total}>$39.80</span>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-            <div className={styles.right}>
-                <div className={styles.wrapper}>
-                    <h2 className={styles.title}>CART TOTAL</h2>
-                    <div className={styles.totalText}>
-                        <b className={styles.totalTextTittle}>Subtotal:</b>$79.60
-                    </div>
-                    <div className={styles.totalText}>
-                        <b className={styles.totalTextTittle}>Discount:</b>$0.00
-                    </div>
-                    <div className={styles.totalText}>
-                        <b className={styles.totalTextTittle}>Total:</b>$79.60
-                    </div>
-                    <button className={styles.button}>CHECKOUT NOW!</button>
-                </div>
-            </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <table className={styles.table}>
+          <tr className={styles.trTitle}>
+            <th>Product</th>
+            <th>Name</th>
+            <th>Extras</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Total</th>
+          </tr>
+          <tr className={styles.tr}>
+            <td>
+              <div className={styles.imgContainer}>
+                <Image
+                  src="/img/product.png"
+                  layout="fill"
+                  objectFit="cover"
+                  alt=""
+                />
+              </div>
+            </td>
+            <td>
+              <span className={styles.name}>Product 1</span>
+            </td>
+            <td>
+              <span className={styles.extras}>
+                Extra Gifts, more balloons
+              </span>
+            </td>
+            <td>
+              <span className={styles.price}>$19.90</span>
+            </td>
+            <td>
+              <span className={styles.quantity}>2</span>
+            </td>
+            <td>
+              <span className={styles.total}>$39.80</span>
+            </td>
+          </tr>
+          <tr className={styles.tr}>
+            <td>
+              <div className={styles.imgContainer}>
+                <Image
+                  src="/img/product.png"
+                  layout="fill"
+                  objectFit="cover"
+                  alt=""
+                />
+              </div>
+            </td>
+            <td>
+              <span className={styles.name}>Product 2</span>
+            </td>
+            <td>
+              <span className={styles.extras}>
+                Extra Gifts, more balloons
+              </span>
+            </td>
+            <td>
+              <span className={styles.price}>$19.90</span>
+            </td>
+            <td>
+              <span className={styles.quantity}>2</span>
+            </td>
+            <td>
+              <span className={styles.total}>$39.80</span>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.wrapper}>
+          <h2 className={styles.title}>CART TOTAL</h2>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Subtotal:</b>$79.60
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Discount:</b>$0.00
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Total:</b>$79.60
+          </div>
+          <button className={styles.button}>CHECKOUT NOW!</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,8 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
-      
+      <meta charSet="UTF-8"></meta>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <Head>
         <title>Meriah Cakes and Goodies in Klang Valley</title>
         <meta name="description" content="Best birthday, wedding, corporate gifts in town" />
